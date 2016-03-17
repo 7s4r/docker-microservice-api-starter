d1dc207b68bc21ab9118e82c3f8db94836066bc3
Gueorgui AGAPOV <7s4r@users.noreply.github.com>
2016-03-17 15:19:27 +0100

    updated config

--- install.sh
+++ install.sh
@@ -4,23 +4,20 @@
 sudo apt-get install -y vagrant
 vagrant box add debian/jessie64 https://dl.dropboxusercontent.com/u/3523744/boxes/debian-8.1-amd64-lxc-puppet/debian-8.1-lxc-puppet.box
 
+# launch vagrant box
+vagrant up
+vagrant ssh
+
 # install & run docker
-sudo apt-get install -y apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 sudo apt-get update
-sudo apt-get purge lxc-docker
-sudo apt-get install -y apparmor
-sudo apt-get install -y docker-engine
+sudo apt-get install -y curl vim
+curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker $USER
 # docker run hello-world
 
 # install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# launch vagrant box
-vagrant up
+sudo apt-get install python-pip
+sudo pip install docker-compose
 
 # install docker containers
 docker-compose up -d
